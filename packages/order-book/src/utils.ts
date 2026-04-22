e3945bc32e1a8c86b72361207ce88f63112d8e8e
Vitalie Andries <vitalie.andries@gmail.com>
2026-04-22 20:40:08 +0300

    chore: Remove unused code

--- packages/order-book/src/utils.ts
+++ packages/order-book/src/utils.ts
@@ -47,30 +47,6 @@ export function isOperationVisible(
     return visibleOperation === "both" || visibleOperation === operation;
 }
 
-export function getAskHoverState(index: number, hoveredAskIndex: number | null) {
-    if (hoveredAskIndex == null) {
-        return "idle" as const;
-    }
-
-    if (index === hoveredAskIndex) {
-        return "active" as const;
-    }
-
-    return "idle" as const;
-}
-
-export function getBidHoverState(index: number, hoveredBidIndex: number | null) {
-    if (hoveredBidIndex == null) {
-        return "idle" as const;
-    }
-
-    if (index === hoveredBidIndex) {
-        return "active" as const;
-    }
-
-    return "idle" as const;
-}
-
 export function createDisplayRows(args: {
     depthMode: DepthMode;
     levels: OrderBookLevel[];
